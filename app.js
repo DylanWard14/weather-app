97727531a84cb9fedf25621cf6a46428d03c7a11
DylanWard14 <dylan.ward@iinet.net.au>
2019-03-22 11:40:21 +1000

    User now has the ability to request a location of their choosing

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const request = require('request');
 const geocode = require("./utils/geocode.js");
 const forecast = require("./utils/forcast"); 
 
-geocode('Boston', (error, data) => {
-    console.log("Error: ", error);
-    console.log("Data ", data);
-})
+const address = process.argv[2];
 
+if(!address)
+{
+    console.log("Please provide an address");
+}
+else
+{
+    geocode(address, (error, data) => {
+        if(error)
+        {
+            return console.log(error);
+        }
+        forecast(data.latitude, data.longitude, (error, forcastData) => {
+            if(error)
+            {
+                return console.log(error);
+            }
 
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
+            console.log(data.location);
+            console.log(forcastData);
+        })
+    })
+}
